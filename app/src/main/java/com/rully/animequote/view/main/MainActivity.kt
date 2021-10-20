e94093c62ba21477d0b5cde06818c09f0c2c6dcc
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-10-20 21:23:19 +0700

    first init

---
+++ app/src/main/java/com/rully/animequote/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rully.animequote.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rully.animequote.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
